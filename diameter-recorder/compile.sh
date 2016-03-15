d46ca5df32fc0af4a3a84ffbbc1fc60c68c47d9c
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2016-03-15 12:03:34 +0100

    Extract diameter and timestamp from JSON data

--- diameter-recorder/compile.sh
+++ diameter-recorder/compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gcc -W -Wall -o diameter-recorder -O2 $(pkg-config --cflags --libs glib-2.0 libczmq) diameter-recorder.c
+gcc -W -Wall -o diameter-recorder -O2 $(pkg-config --cflags --libs libczmq glib-2.0 json-glib-1.0) diameter-recorder.c
--- diameter-recorder/diameter-recorder.c
+++ diameter-recorder/diameter-recorder.c
@@ -1,4 +1,5 @@
 #include <glib.h>
+#include <json-glib/json-glib.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -77,6 +78,77 @@ receive_pupil_message (void  *socket,
 	return TRUE;
 }
 
+/* Parses the JSON data to extract the diameter of the pupil (in pixels), and
+ * the timestamp.
+ * Returns TRUE if successful.
+ * The value -1.0 is set to *diameter_px and *timestamp if the value doesn't
+ * exist in the JSON data.
+ */
+static gboolean
+parse_json_data (const char *json_data,
+		 double     *diameter_px,
+		 double     *timestamp)
+{
+	JsonParser *parser;
+	JsonNode *root_node;
+	JsonArray *array;
+	JsonNode *element_node;
+	JsonObject *object;
+	GError *error = NULL;
+
+	g_assert (diameter_px != NULL);
+	g_assert (timestamp != NULL);
+
+	*diameter_px = -1.0;
+	*timestamp = -1.0;
+
+	parser = json_parser_new ();
+	json_parser_load_from_data (parser, json_data, -1, &error);
+
+	if (error != NULL)
+	{
+		g_warning ("Error when parsing JSON data: %s", error->message);
+		g_clear_error (&error);
+		return FALSE;
+	}
+
+	root_node = json_parser_get_root (parser);
+	if (json_node_get_node_type (root_node) != JSON_NODE_ARRAY)
+	{
+		g_warning ("Error: JSON root node must be an array");
+		return FALSE;
+	}
+
+	array = json_node_get_array (root_node);
+	if (json_array_get_length (array) != 1)
+	{
+		g_warning ("Error: JSON array must have only one element");
+		return FALSE;
+	}
+
+	element_node = json_array_get_element (array, 0);
+	if (json_node_get_node_type (element_node) != JSON_NODE_OBJECT)
+	{
+		g_warning ("Error: expected an object inside the array");
+		return FALSE;
+	}
+
+	object = json_node_get_object (element_node);
+
+	if (json_object_has_member (object, "diameter"))
+	{
+		*diameter_px = json_object_get_double_member (object, "diameter");
+	}
+	if (json_object_has_member (object, "timestamp"))
+	{
+		*timestamp = json_object_get_double_member (object, "timestamp");
+	}
+
+	g_object_unref (parser);
+
+	return TRUE;
+}
+
 int
 main (void)
 {
@@ -101,14 +173,24 @@ main (void)
 	{
 		char *topic = NULL;
 		char *json_data = NULL;
+		double diameter_px;
+		double timestamp;
 
 		if (!receive_pupil_message (subscriber, &topic, &json_data))
 		{
 			g_error ("A Pupil message must be in two parts.");
 		}
 
+		if (!parse_json_data (json_data, &diameter_px, &timestamp))
+		{
+			g_error ("Failed to parse the JSON data.");
+		}
+
 		printf ("Topic: %s\n", topic);
 		printf ("JSON data: %s\n", json_data);
+		printf ("diameter: %lf\n", diameter_px);
+		printf ("timestamp: %lf\n", timestamp);
+		printf ("\n");
 
 		free (topic);
 		free (json_data);
