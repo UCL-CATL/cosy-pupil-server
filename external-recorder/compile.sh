03f217474c7c2feb5b411dde88b43fc771a52ca6
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2016-05-02 14:39:44 +0200

    build: replace compile.sh scripts by Makefiles
    A Makefile is more standard.

--- external-recorder/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-gcc -W -o external-recorder -O2 $(pkg-config --cflags --libs libczmq glib-2.0 json-glib-1.0) external-recorder.c
--- tests/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-gcc -W -o test-request -O2 $(pkg-config --cflags --libs libczmq) test-request.c
