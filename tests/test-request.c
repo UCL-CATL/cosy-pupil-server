7837b9f231878e6d08af96f2d1253fdffb47e467
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2017-03-21 16:48:39 +0100

    test-request: cleanup, use better utility function

--- tests/test-request.c
+++ tests/test-request.c
@@ -1,10 +1,42 @@
 /* Request-reply test */
 
+#include <glib.h>
 #include <zmq.h>
 #include <string.h>
-#include <stdio.h>
 #include <unistd.h>
-#include "zhelpers.h"
+
+/* Receives the next zmq message part as a string.
+ * Free the return value with g_free() when no longer needed.
+ */
+static char *
+receive_next_message (void *socket)
+{
+	zmq_msg_t msg;
+	int n_bytes;
+	char *str = NULL;
+	int ok;
+
+	ok = zmq_msg_init (&msg);
+	g_return_val_if_fail (ok == 0, NULL);
+
+	n_bytes = zmq_msg_recv (&msg, socket, 0);
+	if (n_bytes > 0)
+	{
+		void *raw_data;
+
+		raw_data = zmq_msg_data (&msg);
+		str = g_strndup (raw_data, n_bytes);
+	}
+
+	ok = zmq_msg_close (&msg);
+	if (ok != 0)
+	{
+		g_free (str);
+		g_return_val_if_reached (NULL);
+	}
+
+	return str;
+}
 
 int
 main (void)
@@ -13,7 +45,6 @@ main (void)
 	void *requester;
 	char *request;
 	char *reply;
-	int i;
 
 	context = zmq_ctx_new ();
 
@@ -24,9 +55,9 @@ main (void)
 	printf ("Send request: %s ...\n", request);
 	zmq_send (requester, request, strlen (request), 0);
 
-	reply = s_recv (requester);
+	reply = receive_next_message (requester);
 	printf ("Reply received: %s\n", reply);
-	free (reply);
+	g_free (reply);
 
 	sleep (10);
 
@@ -34,9 +65,9 @@ main (void)
 	printf ("Send request: %s ...\n", request);
 	zmq_send (requester, request, strlen (request), 0);
 
-	reply = s_recv (requester);
+	reply = receive_next_message (requester);
 	printf ("Reply received: %s\n", reply);
-	free (reply);
+	g_free (reply);
 
 	zmq_close (requester);
 	zmq_ctx_destroy (context);
