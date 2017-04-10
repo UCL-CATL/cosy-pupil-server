13bdd248f3e307b59134c76a1c0ada7c40c059d0
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2017-04-10 12:22:31 +0200

    Fix kill-all-containers.sh in case there are no containers to kill

--- kill-all-containers.sh
+++ kill-all-containers.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-sudo docker stop $(sudo docker ps -a -q)
-sudo docker rm $(sudo docker ps -a -q)
+list=$(sudo docker ps -a -q)
+num=$(sudo docker ps -a -q | wc -l)
+
+if [ "$num" -eq "0" ]; then
+	echo "OK, there were no containers to kill."
+	exit 0
+fi
+
+sudo docker stop $list
+sudo docker rm $list
+echo "OK, $num container(s) have been killed."
