81919139dd8fc824461ec6f02e49247eb19c6c49
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2017-04-25 14:39:59 +0200

    ansible: use lineinfile module for configuring sshd

--- ansible/roles/ssh/tasks/configure-sshd.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Disable ssh password authentication. Must authenticate with an authorized key.
-sed -i 's/^PasswordAuthentication yes$/PasswordAuthentication no/' /etc/ssh/sshd_config
