00442e18ae1d7aa0d3b6daa837ebfe98eaee7b36
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2017-05-09 10:31:50 +0200

    Remove Ansible files, moved to cosy-linux-automation repo

--- ansible/ssh-copy-id.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "user:"
-ssh-copy-id user@130.104.191.153
-ansible catl-test -a "cat ~/.ssh/authorized_keys" -u user
-
-echo "root:"
-ssh-copy-id root@130.104.191.153
-ansible catl-test -a "cat ~/.ssh/authorized_keys" -u root
